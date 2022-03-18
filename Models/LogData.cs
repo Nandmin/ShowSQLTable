303b958cd281ced17c641e254f3991ede3a225e5
nemet <nemet@DEVELOPERPC03>
2022-03-18 13:42:19 +0100

    Add project files.

---
+++ Models/LogData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShowSQLTable.Models
+{
+    public class LogData
+    {
+        public string SiteID { get; set; }
+        public string ID { get; set; }
+        public string Login { get; set; }
+        public string Title { get; set; }
+        public string Email { get; set; }
+        public string ExternalTokenLastUpdated { get; set; }
+        public string Mobile { get; set; }
+
+    }
+}
---
+++ Models/UserData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShowSQLTable.Models
+{
+    public class UserData
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+
+    }
+}
